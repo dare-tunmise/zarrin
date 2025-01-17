c3d02380bb550a8cc850d50c1f840331551fc82c
dare-tunmise <daretunmise@yahoo.com>
2025-01-17 06:01:45 +0100

    ...

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -37,8 +37,26 @@ const Home = () => {
             {/* latest post */}
             <div className="latest">
                <div className="header">
-                    <h5>DEVELOPMENT</h5>
-                    <span>16 March 2023</span>
+                    <h1>Our Recent Post</h1>
+                    <a href="">View all</a>
+                </div>
+                <div className="grid-container">
+                    <div className="grid-item item1">
+                        <div className='item1-img'><img src={Vrface} alt="" /></div>
+                        <div className='item1-content'>
+                            <div className="header">
+                                <h5>DEVELOPMENT</h5>
+                                <span>16 March 2023</span>
+                            </div>
+                            <h2>How to make a Game look more attractive with New VR & AI Technology</h2>
+                           <p>Google has been investing in AI for many years and bringing its benefits to individuals, businesses and communities. Whether it’s publishing state-of-the-art research, building helpful products or developing tools and resources that enable others, we’re committed to making AI accessible to everyone.
+                          </p>
+                           <a href="">Read More</a>
+                        </div>
+                    </div>
+                    <div class="grid-item item2">2</div>
+                    <div class="grid-item item3">3</div>
+                    <div class="grid-item item4">4</div>
                 </div>
             </div>
         </>
