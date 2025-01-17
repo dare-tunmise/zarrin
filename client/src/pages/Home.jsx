92118930efcd7ec1952cbadb01a86bfbb8472470
dare-tunmise <daretunmise@yahoo.com>
2025-01-17 08:10:48 +0100

    ...

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import Heroimage from '../assets/hero-image.png'
 import Vrface from '../assets/vrface.jpg'
+import Beach from '../assets/beach.jpg'
 const Home = () => {
     
     return ( 
@@ -55,7 +56,17 @@ const Home = () => {
                         </div>
                     </div>
                     <div class="grid-item item2">
-                      {/* <div className='item1-img'><img src={Vrface} alt="" /></div> */}
+                        <div className='item1-img'><img src={Beach} alt="" /></div>
+                        <div className='item1-content'>
+                            <div className="header">
+                                <h5>DEVELOPMENT</h5>
+                                <span>16 March 2023</span>
+                            </div>
+                            <h2>How to make a Game look more attractive with New VR & AI Technology</h2>
+                           <p>Google has been investing in AI for many years and bringing its benefits to individuals, businesses and communities. Whether it’s publishing state-of-the-art research, building helpful products or developing tools and resources that enable others, we’re committed to making AI accessible to everyone.
+                          </p>
+                           <a href="">Read More</a>
+                        </div>
                     </div>
                     <div class="grid-item item3">3</div>
                     <div class="grid-item item4">4</div>
