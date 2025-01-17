a09beb7fc83824dcfa1db416f859f70bb8bbcbc6
dare-tunmise <daretunmise@yahoo.com>
2025-01-17 04:57:28 +0100

    ...

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -16,15 +16,29 @@ const Home = () => {
                     <img src={Heroimage} alt="" />
                 </div>
             </div>
-
+            {/* feature */}
             <div className="features">
                 <div className="feature">
                     <div className="featured-img">
                         <img src={Vrface} alt="" />
                     </div>
                     <div className="featured-post">
-
+                        <div className="header">
+                            <h5>DEVELOPMENT</h5>
+                            <span>16 March 2023</span>
+                        </div>
+                        <h2>How to make a Game look more attractive with New VR & AI Technology</h2>
+                        <p>Google has been investing in AI for many years and bringing its benefits to individuals, businesses and communities. Whether it’s publishing state-of-the-art research, building helpful products or developing tools and resources that enable others, we’re committed to making AI accessible to everyone.
+                        </p>
+                        <a href="">Read More</a>
                     </div>
+                </div>
+            </div>
+            {/* latest post */}
+            <div className="latest">
+               <div className="header">
+                    <h5>DEVELOPMENT</h5>
+                    <span>16 March 2023</span>
                 </div>
             </div>
         </>
