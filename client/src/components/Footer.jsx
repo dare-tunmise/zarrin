3d9fec4a7cdcf6410ed6a3f15696f12af2adbc85
dare-tunmise <daretunmise@yahoo.com>
2025-01-25 18:34:44 +0100

    ...

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,3 +1,5 @@
+import zarrinLogo from '../assets/zarrinLogo.svg'
+
 const Footer = () => {
     return ( 
         <footer>
@@ -9,6 +11,16 @@ const Footer = () => {
                 </div>
                 <p>Get a response tomorrow if you submit by 9pm today. If we received after 9pm will get a reponse the following day.</p>
             </div>
+            <div className="logo">
+                <img src={zarrinLogo} alt="" />
+                <span>Zarrin</span>
+            </div>
+            <ul>
+                <li><a href="">Home</a></li>
+                <li><a href="">Blog</a></li>
+                <li><a href="">About</a></li>
+                <li><a href="">Contact Us</a></li>
+            </ul>
 
         </footer>
      );
