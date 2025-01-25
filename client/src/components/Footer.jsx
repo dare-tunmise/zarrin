5c7d6bd77da0049f25c8b57dde9b228f61fb7041
dare-tunmise <daretunmise@yahoo.com>
2025-01-25 15:25:20 +0100

    ...

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,7 +1,15 @@
 const Footer = () => {
     return ( 
         <footer>
-            
+            <div className="footer-one">
+                <h2>Get our stories delivered From us to your inbox weekly.</h2>
+                <div className="form">
+                    <input type="email" name="" placeholder="Your Email" id="" />
+                    <button>Get Started</button>
+                </div>
+                <p>Get a response tomorrow if you submit by 9pm today. If we received after 9pm will get a reponse the following day.</p>
+            </div>
+
         </footer>
      );
 }
